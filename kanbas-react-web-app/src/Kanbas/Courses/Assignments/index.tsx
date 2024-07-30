323bbd4d346de8ca5ad1906504ac4187a4d5ea7d
lucasmaochenyu <75525496+lucasmaochenyu@users.noreply.github.com>
2024-07-29 22:11:12 -0700

    lab4 and a4

--- kanbas-react-web-app/src/Kanbas/Courses/Assignments/index.tsx
+++ kanbas-react-web-app/src/Kanbas/Courses/Assignments/index.tsx
@@ -1,6 +1,6 @@
+import AssignmentControls from "./AssignmentCenters";
 import { CiSearch } from "react-icons/ci";
-import AssignmentCenters from "./AssignmentCenters";
-import AssignmentCentersButtons from "./AssignmentCentersButtons";
+import AssignmentControlButtons from "./AssignmentCentersButtons";
 import { BsGripVertical } from "react-icons/bs";
 import { GiNotebook } from "react-icons/gi";
 import { IoEllipsisVertical } from "react-icons/io5";
@@ -8,28 +8,37 @@ import { FaPlus } from "react-icons/fa6";
 import { VscTriangleDown } from "react-icons/vsc";
 import { useParams, useLocation } from "react-router";
 import { Link } from 'react-router-dom';
-import { assignments } from "../../Database";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteAssignment } from "./reducer";
 
 export default function Assignments() {
     const { cid } = useParams();
-    const courseAssignments = assignments.filter((assignment) => assignment.course === cid);
     const { pathname } = useLocation();
     const course = pathname.split("/")[3];
+    const { assignments } = useSelector((state: any) => state.assignmentsReducer);
+    const dispatch = useDispatch();
+    const currentDate = new Date();
+    const formatDate = (dateString: String) => {
+        if (!dateString) return 'N/A';
+        return dateString.split('T');
+    };
+    const gradeUnit = (displayGradeAs: String) => {
+        if (displayGradeAs === "Percentage") return "%";
+        else return "pts";
+    }
 
     return (
         <div id="wd-assignments">
             <div className="d-flex justify-content-between align-items-center mb-3">
-                <div className="input-group" style={{ width: '100px' }}>
-                    <span className="input-group-text">
+                <div className="input-group" style={{ width: '200px' }}>
+                    <span className="input-group-text" style={{ backgroundColor: 'transparent' }}>
                         <CiSearch />
                     </span>
-                    <input 
-                        type="text"
-                        id="wd-search-assignment"
+                    <input id="wd-search-assignment"
                         className="form-control"
                         placeholder="Search..." />
                 </div>
-                <AssignmentCenters />
+                <AssignmentControls course={course} />
             </div>
 
             <ul id="wd-modules" className="list-group rounded-0">
@@ -48,27 +57,36 @@ export default function Assignments() {
                     </div>
 
                     <ul className="wd-lessons list-group rounded-0">
-                        {courseAssignments.map((assignment) => (
-                            <li className="wd-lesson list-group-item p-3 ps-1" style={{ borderLeft: '5px solid green' }}>
-                                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
-                                    <div style={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
-                                        <BsGripVertical className="me-2" style={{ fontSize: '1.5rem' }} />
-                                        <GiNotebook style={{ color: 'green', marginRight: '12px', fontSize: '1.5rem' }} />
-                                        <div style={{ margin: '0 20px' }}>
-                                            <Link to={`/Kanbas/Courses/${course}/Assignments/${assignment._id}`} 
-                                            style={{ color: 'black', textDecoration: 'none', fontWeight: 'bold' }}>
-                                                {assignment._id} {assignment.title}
-                                            </Link>
-                                            <br />
-                                            <span>
-                                                <span className="text-danger">Multiple Modules</span> | <b> Not available until</b> May 6 at 12:00am | <b> Due </b> May 13 at 11:59pm | 100 pts
-                                            </span>
+                        {assignments
+                            .filter((assignment: any) => assignment.course === cid)
+                            .map((assignment: any) => (
+                                <li className="wd-lesson list-group-item p-3 ps-1" style={{ borderLeft: '5px solid green' }}>
+                                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+                                        <div style={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
+                                            <BsGripVertical className="me-2" style={{ fontSize: '1.5rem' }} />
+                                            <GiNotebook style={{ color: 'green', marginRight: '12px', fontSize: '1.5rem' }} />
+                                            <div style={{ margin: '0 20px' }}>
+                                                <Link to={`/Kanbas/Courses/${course}/Assignments/${assignment._id}`}
+                                                    style={{ color: 'black', textDecoration: 'none', fontWeight: 'bold' }}>
+                                                    {assignment._id} {assignment.title}
+                                                </Link>
+                                                <br />
+                                                <span>
+                                                    <span className="text-danger">Multiple Modules</span> | <b> Not available until</b> {formatDate(assignment.from)[0]} {formatDate(assignment.from)[1]} | <b> Due </b> {formatDate(assignment.due)[0]} {formatDate(assignment.from)[1]} | {assignment.points} {gradeUnit(assignment.displayGradeAs)}
+                                                </span>
+                                            </div>
                                         </div>
+                                        <AssignmentControlButtons assignmentId={assignment._id}
+                                            deleteAssignment={(assignmentId) => {
+                                                dispatch(deleteAssignment(assignmentId));
+                                            }}
+
+
+                                        />
+
                                     </div>
-                                    <AssignmentCentersButtons />
-                                </div>
-                            </li>
-                        ))}
+                                </li>
+                            ))}
                     </ul>
                 </li>
             </ul>
