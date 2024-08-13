2d05f04336138e221b2de206b92091ef66f2469a
lucasmaochenyu <75525496+lucasmaochenyu@users.noreply.github.com>
2024-08-13 10:28:21 -0700

    change the remote server link

--- kanbas-node-server-app/App.js
+++ kanbas-node-server-app/App.js
@@ -1,16 +1,57 @@
+import "dotenv/config";
 import express from 'express';
-import Hello from "./Hello.js"
-import Lab5 from "./Lab5/index.js";
+import Hello from "./Hello.js";
+import Lab5 from './Lab5/index.js';
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from './Kanbas/Assignments/routes.js'
-import cors from "cors"
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import UserRoutes from "./Users/routes.js"
+import cors from "cors";
+import mongoose from 'mongoose';
+import session from "express-session";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express()
-app.use(cors()); 
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    }));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(
+    session(sessionOptions));
+
 app.use(express.json());
-Hello(app)
-CourseRoutes(app);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use('/images', express.static(path.join(__dirname, '../kanbas-react-web-app/public/images')));
+
+
+UserRoutes(app);
 ModuleRoutes(app);
-AssignmentRoutes(app)
+CourseRoutes(app);
+AssignmentRoutes(app);
+
+Hello(app);
 Lab5(app);
+
 app.listen(process.env.PORT || 4000)
