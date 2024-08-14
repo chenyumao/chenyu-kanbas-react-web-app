5cd901b35ae8800bd3002a9f47ec3094fa20330d
lucasmaochenyu <75525496+lucasmaochenyu@users.noreply.github.com>
2024-08-14 15:51:07 -0700

    a6 server code

---
+++ kanbas-node-server-app/Kanbas/Courses/routes.js
@@ -0,0 +1,47 @@
+import * as dao from "./dao.js";
+
+export default function CourseRoutes(app) {
+    
+    app.put("/api/courses/:id", async (req, res) => {
+        const { id } = req.params;
+        const course = req.body;
+        try {
+            await dao.updateCourse(id, course);
+            res.sendStatus(204);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+
+   
+    app.delete("/api/courses/:id", async (req, res) => {
+        const { id } = req.params;
+        try {
+            await dao.deleteCourse(id);
+            res.sendStatus(204);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+
+   
+    app.post("/api/courses", async (req, res) => {
+        const course = req.body;
+        try {
+            const newCourse = await dao.createCourse(course);
+            res.json(newCourse);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+
+    app.get("/api/courses", async (req, res) => {
+        try {
+            const courses = await dao.findAllCourses();
+            res.json(courses);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+
+}
--- kanbas-react-web-app/src/Kanbas/Courses/client.ts
+++ kanbas-react-web-app/src/Kanbas/Courses/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
-
+console.log(COURSES_API)
 // Retrieving Courses
 export const fetchAllCourses = async () => {
     const response = await axios.get(COURSES_API);
